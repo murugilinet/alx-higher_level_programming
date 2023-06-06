0043ee9bd73b5d49a2fc599bfcb497ff19c4d7e1
murugilinet <murugilinet59@gmail.com>
2023-06-06 13:23:22 -0700

    updated tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 output = ""
 for character in range(97, 123):
     if character != 101 and character != 113:
-        output  += "{:c}".format(character)
+        output += "{:c}".format(character)
 print(output, end="")
