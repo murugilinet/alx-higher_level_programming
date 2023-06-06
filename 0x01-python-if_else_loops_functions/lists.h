0c1b9fede960ca394c64984395b7c35affc2abd6
murugilinet <murugilinet59@gmail.com>
2023-06-06 11:36:47 -0700

    task1

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def islower(c);
+def uppercase(str);
+def print_last_digit(number);
+def add(a, b);
+def pow(a, b);
+listint_t *insert_node(listint_t **head, int number);
+
+#endif/*LISTS_H*/
